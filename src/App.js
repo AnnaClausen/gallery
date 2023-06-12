6ae2669d723a7575c0f74d67d15bcf317cf4e8fc
AnnaRClausen <annaclausen206@gmail.com>
2023-06-11 18:48:47 -0700

    create upload form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import UploadForm from './components/UploadForm';
 import './App.css';
 
 const images = [
-  'https://picsum.photos/id/10/200/200',
-  'https://picsum.photos/id/22/200/200',
-  'https://picsum.photos/id/32/200/200',
-  'https://picsum.photos/id/38/200/200',
-  'https://picsum.photos/id/57/200/200',
-  'https://picsum.photos/id/59/200/200',
-  'https://picsum.photos/id/77/200/200',
-  'https://picsum.photos/id/78/200/200',
-  'https://picsum.photos/id/82/200/200'
+  'https://picsum.photos/id/10/200/200'
 
 ]
 
 function App() {
+  const [count, setCount] = useState();
+  const [inputs, setInputs] = useState({ title: null, file: null, path:null });
   const [items, setItems] = useState(images);
+  const [isCollapsed, collapse] = useState(false);
+
+  const toggle = () => collapse(!isCollapsed);
+  const handleOnChange = (e) => {
+    if (e.target.name === 'file') {
+    setInputs({ ...inputs, title: e.target.value, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0]) });
+    } else {
+      setInputs({ ...inputs, title: e.target.value})
+    }
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setItems([inputs.path, ...items]);
+  }
+
+  useEffect(() => {
+    setCount(`you have ${items.length} image${items.length > 1 ? 's' : ''}`);
+
+  }, [items]);
+
   return (
     <>
     <Navbar />
     <div class="container text-center mt-5">
-      <button className='btn btn-warning' onClick={() => setItems(['https://picsum.photos/id/82/200/200', ...items])}>+Add</button>
+      <button className='btn btn-success mx-5 float-end' onClick={toggle}>{ isCollapsed ? 'Close' : '+ Add' }</button>
+      <div className="clearfix mb-4"></div>
+      <UploadForm 
+      isVisible={isCollapsed}
+      onChange={handleOnChange}
+      onSubmit={handleOnSubmit}
+      />
+      {count}
       <h1>Gallery</h1>
       <div className="row">
         {items.map((image) => <Card src={image} />)}
@@ -33,4 +55,3 @@ function App() {
 }
 
 export default App;
-
